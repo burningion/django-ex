6aa5c47dac24542466be98231b153753cec99cf9
KP Kaiser <kirk@zothcorp.com>
2018-05-24 17:14:44 -0400

    Reset to working one more time

--- app.py
+++
@@ -1,5 +0,0 @@
-from subprocess import call
-import os
-
-command = '/opt/app-root/bin/ddtrace-run gunicorn wsgi:application --bind=0.0.0.0:8080 --access-logfile=-' 
-call(command.split(' '), shell=True)

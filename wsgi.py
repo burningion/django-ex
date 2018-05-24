769c9944c5541b8b720f37c3ad1165f71f12a82b
KP Kaiser <kirk@zothcorp.com>
2018-05-24 16:39:16 -0400

    No env variables until after successful build with daemonset

--- wsgi.py
+++ wsgi.py
@@ -10,12 +10,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import ddtrace
 
+'''
 ddtrace.tracer.configure(
     hostname=os.environ['DD_AGENT_SERVICE_HOST'],
     port=os.environ['DD_AGENT_SERVICE_PORT'],
 )
 
-
+'''
 ddtrace.patch_all()
 
 from django.core.wsgi import get_wsgi_application

8bc23ca8db8d32fa1ed15c62cd9187348487cec8
KP Kaiser <kirk@zothcorp.com>
2018-05-24 17:25:56 -0400

    Enable debug, and what happens when you enable tracer in multiple places

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from subprocess import call
 import os
 
-command = '/opt/app-root/bin/ddtrace-run gunicorn wsgi:application --bind=0.0.0.0:8080 --access-logfile=-' 
+command = 'DATADOG_TRACE_DEBUG=true /opt/app-root/bin/ddtrace-run gunicorn wsgi:application --bind=0.0.0.0:8080 --access-logfile=-' 
 call(command.split(' '))

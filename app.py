1d9905f9db964f3ad980a9327c7d6826ff6a43cf
KP Kaiser <kirk@zothcorp.com>
2018-05-24 16:25:11 -0400

    Try removing quotes

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 from subprocess import call
-command = 'gunicorn "$APP_MODULE" --bind=0.0.0.0:8080 --access-logfile=- --config "$APP_CONFIG"'
+command = 'gunicorn $APP_MODULE --bind=0.0.0.0:8080 --access-logfile=- --config $APP_CONFIG'
 call(command.split(' '), shell=True)

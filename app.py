ad1f80a4a04d8fde5e636e1f76b2e32cd85db115
KP Kaiser <kirk@zothcorp.com>
2018-05-25 12:18:03 -0400

    Start to finish should run now

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from subprocess import call
 import os
 
+# Set this to be the name of the Agent service defined in your Agent YAML
+os.environ['DATADOG_TRACE_AGENT_HOSTNAME'] = 'dd-agent'
+
+# Run gunicorn wrapped in ddtrace-run
 command = '/opt/app-root/bin/ddtrace-run gunicorn wsgi:application --bind=0.0.0.0:8080 --access-logfile=-' 
+
 call(command.split(' '))

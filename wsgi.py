b2946d04c18fc3b858917a8c3ee79c7c8d621f96
KP Kaiser <kirk@zothcorp.com>
2018-05-25 16:32:08 -0400

    More api jumps

--- wsgi.py
+++ wsgi.py
@@ -8,18 +8,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-import ddtrace
+from ddtrace import tracer
+os.environ['DATADOG_TRACE_AGENT_HOSTNAME'] = os.environ['KUBERNETES_SERVICE_HOST']
 
-
-ddtrace.tracer.configure(
-    hostname=os.environ['KUBERNETES_SERVICE_HOST'],
-)
-
-ddtrace.tracer.set_level('debug')
+# ddtrace.tracer.hostname = os.environ['KUBERNETES_SERVICE_HOST']
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-ddtrace.patch_all()
+tracer.patch_all()
 
 application = get_wsgi_application()

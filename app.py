1222a87d6661b84cecc4180db50471c3a4060e62
KP Kaiser <kirk@zothcorp.com>
2018-05-25 15:21:15 -0400

    Add the kubernetes service host as the main trace hostname

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from subprocess import call
 import os
 
 # Set this to be the name of the Agent service defined in your Agent YAML
-os.environ['DATADOG_TRACE_AGENT_HOSTNAME'] = 'dd-agent'
+os.environ['DATADOG_TRACE_AGENT_HOSTNAME'] = os.environ['KUBERNETES_SERVICE_HOST']
 
 # Run gunicorn wrapped in ddtrace-run
 command = '/opt/app-root/bin/ddtrace-run gunicorn wsgi:application --bind=0.0.0.0:8080 --access-logfile=-' 

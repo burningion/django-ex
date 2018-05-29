8282e37cad36cef9a24af263804d390642a23e63
KP Kaiser <kirk@zothcorp.com>
2018-05-29 13:40:08 -0400

    Do manual migration before coming to life

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from subprocess import call
 
 # Run gunicorn wrapped in ddtrace-run
+command = 'python manage.py migrate'
+call(command.split(' '))
 command = '/opt/app-root/bin/ddtrace-run gunicorn wsgi:application --bind=0.0.0.0:8080 --access-logfile=-' 
 call(command.split(' '))
